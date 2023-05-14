cd594e601950432d0436866b6b72468a0f6742a9
Silvia Rodríguez Bares <silvibares@gmail.com>
2023-05-14 13:18:46 +0200

    second version of dynamic programming

--- busDynamic.py
+++ busDynamic.py
@@ -1,86 +1,72 @@
 #!/usr/bin/env python
-import math
 import random
 from copy import copy
+import math
 
+import numpy as np
 
 class SeatAllocator:
     def __init__(self, users, seats):
         self.users = users
-        self.seats = seats
+        self.seats = [seat for row in seats for seat in row]
         self.num_users = len(users)
-        self.num_seats = len(seats[0])
-        self.table = [[math.inf for _ in range(self.num_seats)] for _ in range(self.num_users)]
-        self.prev = [[None for _ in range(self.num_seats)] for _ in range(self.num_users)]
-
-    def cost(self, i, j):
-        if not self.seats[i][j]:
-            return math.inf
-        cost = 0
-        for k in range(i):
-            if self.seats[k][j] and self.users[k]['id'] in self.users[i]['cant_sit_with']:
-                cost += 1
-        return cost
+        self.num_seats = len(self.seats)
 
     def solve(self):
-        for j in range(self.num_seats):
-            self.table[0][j] = self.cost(0, j)
+        F = np.zeros((self.num_users + 1, self.num_seats + 1))
+        decision = np.zeros((self.num_users + 1, self.num_seats + 1))
 
-        for i in range(1, self.num_users):
-            for j in range(self.num_seats):
-                c = self.cost(i, j)
-                if c == math.inf:
-                    self.table[i][j] = c
-                    continue
-                for k in range(self.num_seats):
-                    if j == k:
-                        continue
-                    p = self.table[i-1][k] + c
-                    if p < self.table[i][j]:
-                        self.table[i][j] = p
-                        self.prev[i][j] = k
+        for i in range(1, self.num_users + 1):
+            for j in range(1, self.num_seats + 1):
+                if self.seats[j - 1] is None or any(
+                        (u is not None and u['id'] in self.users[i - 1]['cant_sit_with']) for u in self.seats[:j - 1]):
+                    F[i][j] = F[i - 1][j]
+                    decision[i][j] = 0
+                else:
+                    if F[i - 1][j] >= F[i - 1][j - 1] + 1:
+                        F[i][j] = F[i - 1][j]
+                        decision[i][j] = 0
+                    else:
+                        F[i][j] = F[i - 1][j - 1] + 1
+                        decision[i][j] = 1
+                        self.seats[j - 1] = self.users[i - 1]
 
-        # backtrack to find solution
-        j = self.table[self.num_users-1].index(min(self.table[self.num_users-1]))
-        for i in range(self.num_users-1, -1, -1):
-            if j is None:
-                break
-            self.seats[i][j] = self.users[i]
-            j = self.prev[i][j]
+        self.seats = [user if user is not None else {'id': None, 'cant_sit_with': []} for user in self.seats]
 
-        placed_users = [user for row in self.seats for user in row if user is not None]
+        placed_users = [user for user in self.seats if user['id'] is not None]
         unplaced_users = [user for user in self.users if user not in placed_users]
 
-        return self.seats, unplaced_users
+        return self.seats, unplaced_users, decision
+
+
 
 if __name__ == '__main__':
-    # Example usage:
     users = [
         {"id": 1, "cant_sit_with": [2, 3]},
         {"id": 2, "cant_sit_with": [1, 8, 4]},
         {"id": 3, "cant_sit_with": [2, 1]},
         {"id": 4, "cant_sit_with": [2, 8]},
-        {"id": 5, "cant_sit_with": [2, 4, 6]},
+        {"id": 5, "cant_sit_with": [2, 4,        6]},
         {"id": 6, "cant_sit_with": [2]},
         {"id": 7, "cant_sit_with": [2, 1, 4, 6]},
         {"id": 8, "cant_sit_with": [2]}
     ]
 
-    seats = [[None for _ in range(3)] for _ in range(8)]
+    seats = [[None, 0, None, None, 0], [0, 0, None], [0, 0, None], [0, 0, None]]
 
     sa = SeatAllocator(users, seats)
-    solution, unplaced_users = sa.solve()
+    solution, unplaced_users, decision = sa.solve()
 
     print("Placed users:")
-    for row in solution:
-        for user in row:
-            if user is None:
-                print("| |", end="\t")
-            else:
-                print(f"|{user['id']}|", end="\t")
-        print()
+    for i in range(len(solution)):
+        if solution[i]['id'] is None:
+            print("| |", end="\t")
+        else:
+            print(f"|{solution[i]['id']}|", end="\t")
+        if (i+1) % 5 == 0:  # change to the number of seats in a row
+            print()
 
-# mirar los usuarios que no se han podido sentar
     print("\nUnplaced users:")
     for user in unplaced_users:
         print(f"{user['id']}")
+
